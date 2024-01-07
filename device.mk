630f204e0a76f524d762d4fa5c1dbc8f73b33790
SquareTea(TEA) <kmd36rk@gmail.com>
2024-01-07 09:34:14 +0700

    device.mk: also build  android.hardware.secure_element_snxxx@1.2-service

--- device.mk
+++ device.mk
@@ -281,7 +281,7 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2 \
     android.hardware.nfc@1.2.vendor:64 \
     android.hardware.nfc_snxxx@1.2-service \
-    android.hardware.secure_element@1.2.vendor:64 \
+    android.hardware.secure_element_snxxx@1.2-service \
     com.android.nfc_extras \
     NfcNci \
     Tag \
